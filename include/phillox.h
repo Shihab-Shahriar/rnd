127af22ec5fbc18f65b1148db7e1bb9b4b2e63b4
Shihab Shahriar Khan <khanmd@msu.edu>
2023-07-27 12:55:44 -0400

    code works on gpu, compilation leads to some warnings

--- include/phillox.h
+++ include/phillox.h
@@ -3,6 +3,8 @@
 #include <array>
 #include <limits>
 
+#define DEVICE __host__ __device__
+
 #ifndef PHILOX_M4x32_0
 #define PHILOX_M4x32_0 ((uint32_t)0xD2511F53)
 #endif
@@ -18,41 +20,72 @@
 #endif
 
 
+struct Counter{
+    uint32_t v[4];
 
-inline uint64_t mulhilo(uint32_t L, uint32_t R, uint32_t *hip) {
+    DEVICE Counter(uint32_t a, uint32_t b, uint32_t c, uint32_t d)
+    
+    {
+        v[0] = a;
+        v[1] = b;
+        v[2] = c;
+        v[3] = d;
+    }
+
+    DEVICE uint32_t& operator[](int i) {
+        return v[i];
+    }
+};
+
+
+struct Key{
+    uint32_t v[2];
+
+    DEVICE Key(uint32_t a, uint32_t b){
+        v[0] = a;
+        v[1] = b;
+    }
+
+    DEVICE uint32_t& operator[](int i) {
+        return v[i];
+    }
+};
+
+
+inline DEVICE uint64_t mulhilo(uint32_t L, uint32_t R, uint32_t *hip) {
     uint64_t product = static_cast<uint64_t>(L)*static_cast<uint64_t>(R);
     *hip = product>>32;
     return static_cast<uint32_t>(product);
 }
 
-void bumpkey(std::array<uint32_t, 2>& key ){
+DEVICE void bumpkey(Key& key ){
     key[0] += PHILOX_W32_0;
     key[1] += PHILOX_W32_1;
 }
 
-std::array<uint32_t, 4> round(std::array<uint32_t, 2>& key, std::array<uint32_t, 4>& ctr){
+DEVICE Counter round(Key& key, Counter& ctr){
     uint32_t hi0;                                                              
     uint32_t hi1;                                                              
     uint32_t lo0 = mulhilo(PHILOX_M4x32_0, ctr[0], &hi0);              
     uint32_t lo1 = mulhilo(PHILOX_M4x32_1, ctr[2], &hi1);              
-    return {{hi1^ctr[1]^key[0], lo1, hi0^ctr[3]^key[1], lo0}};           
+    return Counter(hi1^ctr[1]^key[0], lo1, hi0^ctr[3]^key[1], lo0);           
 }
-
 
 
 class Phillox{
     public:
 
-    Phillox(uint64_t seed_, uint64_t counter_) : 
+    DEVICE Phillox(uint64_t seed_, uint64_t counter_) : 
         seed(seed_), counter(counter_)
     {
 
     }
 
 
-    std::array<uint32_t, 4> generate(){
-        std::array<uint32_t, 2> key = {(uint32_t)(seed>>32), (uint32_t)(seed & 0xFFFFFFFF)}; 
-        std::array<uint32_t, 4> ctr = {0, 0, (uint32_t)(counter>>32), (uint32_t)(counter & 0xFFFFFFFF)};
+    DEVICE Counter generate(){
+        Key key{(uint32_t)(seed>>32), (uint32_t)(seed & 0xFFFFFFFF)}; 
+        // use an internal counter for first two numbers
+        Counter ctr{0, 0, (uint32_t)(counter>>32), (uint32_t)(counter & 0xFFFFFFFF)};
 
         for(int r=0; r<10; r++){
             if(r>0) bumpkey(key);
@@ -63,17 +96,16 @@ class Phillox{
     }
 
     template <typename T>
-    T urand(){
-        std::array<uint32_t, 4> out = generate();
+    DEVICE T urand(){
+        Counter out = generate();
         if constexpr (sizeof(T)==4) return static_cast<T>(out[0]);
-        else{
-            T ret = ((T)out[0] << 32) | out[1];
-            return ret;
-        }
+
+        T ret = ((T)out[0] << 32) | out[1];
+        return ret;
     }
 
-    template <typename T>
-    T draw(){
+    template <typename T = int>
+    DEVICE T draw(){
         if constexpr (std::is_integral_v<T>){
             return urand<T>();
         }
@@ -82,14 +114,10 @@ class Phillox{
         }
     }
 
-    void fill_random(int *array, int N){
+    DEVICE void fill_random(int *array, int N){
 
     }
 
     uint64_t seed;
     uint64_t counter;
-
-
-
-
 };
--- tests/test_uniform.cpp
+++ tests/test_uniform.cpp
@@ -4,7 +4,21 @@
 using std::cout;
 using std::endl;
 
-TEST(RNG, BasicAssertions) {
+TEST(RNG, basic) {
+    Phillox rng(42, 0);
+    EXPECT_NE(rng.draw(), rng.draw());
+
+    Phillox rng1(1, 0);
+    Phillox rng2(1, 0);
+    EXPECT_EQ(rng1.draw(),rng2.draw());
+    EXPECT_EQ(rng1.draw(),rng2.draw());
+
+    Phillox rng3(3, 0);
+    Phillox rng4(4, 0);
+    EXPECT_NE(rng3.draw(), rng4.draw()); //this "could" happen :)
+}
+
+TEST(RNG, mean) {
     Phillox rng(42, 0);
     int num_draws = 10000;
 
@@ -16,3 +30,4 @@ TEST(RNG, BasicAssertions) {
     cout<<mean<<endl;
     EXPECT_EQ(7 * 6, 42);
 }
+
