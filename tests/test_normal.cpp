cb0d0455c40b97b2f160fbbaa3ead5faf15b640b
Shihab Shahriar Khan <khanmd@msu.edu>
2023-09-02 02:24:25 -0400

    Added normalcy test for all generators

--- tests/test_normal.cpp
+++ tests/test_normal.cpp
@@ -2,20 +2,78 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <gtest/gtest.h>
 
 #include "phillox.h"
 #include "tyche.h"
+#include "threefry.h"
+#include "squares.h"
 
 using std::cout;
 using std::endl;
 
-int main(){
-    using RNG=Phillox;
-    RNG rng(42, 0);
-    int num_draws = 100000;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <functional>
 
-    for (int i = 0; i < num_draws-1; i++) printf("%f ", rng.randn<float>());
-    printf("%f", rng.randn<float>());
-    return 0;
+double standard_normal_cdf(double x) {
+    return 0.5 * std::erfc(-x * M_SQRT1_2);
 }
+
+std::pair<double, bool> ks_test(const std::vector<double>& data) {
+    int n = data.size();
+    std::vector<double> data_sorted(data);
+    std::sort(data_sorted.begin(), data_sorted.end());
+
+    double max_diff = 0.0;
+
+    for (int i = 0; i < n; ++i) {
+        double cdf_data = static_cast<double>(i + 1) / static_cast<double>(n);
+        double cdf_theoretical = standard_normal_cdf(data_sorted[i]);
+        max_diff = std::max(max_diff, std::abs(cdf_data - cdf_theoretical));
+    }
+
+    // Calculate the critical value
+    double alpha = 0.05;  // significance level
+    double critical_value = std::sqrt(-0.5 * std::log(alpha / 2.0)) * std::sqrt(static_cast<double>(n));
+
+    bool reject_null = max_diff > critical_value;
+
+    return {max_diff, reject_null};
+}
+
+template <typename RNG>
+void test_normalcy(){
+    RNG rng(42, 0);
+    const int num_draws = 5000; // should be more than enough
+
+    std::vector<double> sample_data;
+    for (int i = 0; i < num_draws; i++) 
+        sample_data.push_back(rng.template randn<float>());
+
+    auto [max_diff, reject_null] = ks_test(sample_data);
+
+    std::cout << "Maximum difference between CDFs: " << max_diff << std::endl;
+    ASSERT_TRUE(!reject_null);
+}
+
+TEST(normal, phillox) {
+    test_normalcy<Phillox>();
+}
+
+TEST(normal, tyche) {
+    test_normalcy<Tyche>();
+}
+
+TEST(normal, threefry) {
+    test_normalcy<Threefry>();
+}
+
+TEST(normal, squares) {
+    test_normalcy<Squares>();
+}
+
+
 
