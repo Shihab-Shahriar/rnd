86bb874cdf4c9c885fdecab6a803c0a3307206de
Shihab Shahriar Khan <khanmd@msu.edu>
2023-09-30 13:39:28 -0400

    CI: added `stat_tests` workflow to run statsistical tests on Github

--- tests/run_stat_tests.py
+++ tests/run_stat_tests.py
@@ -1,7 +1,4 @@
 import subprocess
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
 from datetime import datetime
 import os
 
@@ -11,9 +8,10 @@ now = datetime.now()
 # Format and print it
 print("Current date and time:", now.strftime("%Y-%m-%d %H:%M:%S"))
 
-BUILD_DIR = "/home/shihab/src/rnd/build"
-RES_DIR = "/home/shihab/src/rnd/build/results"
-
+ROOT_DIR = os.getcwd()
+BUILD_DIR = os.path.join(ROOT_DIR, "build")
+RES_DIR = os.path.join(BUILD_DIR, "results")
+print(ROOT_DIR, BUILD_DIR, RES_DIR)
 
 procs = []
 # create results directory if not exists
@@ -21,27 +19,32 @@ os.makedirs(RES_DIR, exist_ok=True)
 
 
 # launch practrand multistream tests
-PRACT_RND_EXEC = "/home/shihab/codes/PractRand/RNG_test"
+PRACT_RND_EXEC = os.path.join(BUILD_DIR, "Practrand", "RNG_test")
 
 for gen in ["philox", "tyche", "threefry", "squares"]:
-    command  = f"{BUILD_DIR}/tests/pract_rand_multi {gen} | {PRACT_RND_EXEC} stdin32 -multithreaded -tlmax 16GB > {RES_DIR}/practrandm_{gen}.txt"
+    command  = f"{BUILD_DIR}/tests/pract_rand_multi {gen} | {PRACT_RND_EXEC} stdin32 -multithreaded -tlmax 4GB > {RES_DIR}/practrandm_{gen}.txt"
 
     p = subprocess.Popen(command, shell=True)
+    p.name = f"practrand_{gen}"
     procs.append(p)
 
 
 
 # launch TESTU01 tests
-LEVEL = "small"
+# LEVEL = "crush"
 
-for gen in ["philox", "tyche", "threefry", "squares"]:
-    command  = f"{BUILD_DIR}/tests/testu01_multi {gen} {LEVEL} > {RES_DIR}/testu01m_{gen}.txt"
-    print(command)
-    p = subprocess.Popen(command, shell=True)
-    procs.append(p)
+# for gen in ["philox", "tyche", "threefry", "squares"]:
+#     command  = f"{BUILD_DIR}/tests/testu01_multi {gen} {LEVEL} > {RES_DIR}/testu01m_{gen}.txt"
+#     print(command)
+#     p = subprocess.Popen(command, shell=True)
+#     procs.append(p)
 
 # Wait for them to finish
 for p in procs:
     p.communicate()
+    if p.returncode != 0:
+        print("Error in test ", p.name)
+        exit(1)
+    print(p.name, " finished successfully")   
     now = datetime.now()
     print("Current date and time:", now.strftime("%Y-%m-%d %H:%M:%S"))
